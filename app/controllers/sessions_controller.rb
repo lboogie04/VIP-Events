edaba8431dc6f034fbd20720e23152fc5be3b2ab
Lawrence Secrease <lawrence@bookshout.com>
2017-12-30 23:05:39 -0600

    Refine login process

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,21 @@ class SessionsController < ApplicationController
  end
  
  def create
+  user = User.find_by(name: params[:session][:name])
+  if user 
+   session[:user_id] = user.id
+   flash[:info] = "Sign in successful"
+   redirect_to root_path
+  else 
+   flash[:info] = "Something went wrong"
+   redirect_to root_path
+  end 
  end
  
  def destroy
+  session.delete(:user_id)
+  @current_user = nil
+  redirect_to root_url
  end
  
  
